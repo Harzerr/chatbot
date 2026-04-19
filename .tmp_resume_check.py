9e3f826e1811b8b9d34e4fd2bd94475845bef364
Harzerr <2966063467@qq.com>
2026-04-19 21:22:44 +0800

    419-2122

---
+++ .tmp_resume_check.py
@@ -0,0 +1,11 @@
+﻿import asyncio
+from app.services.resume_parser import ResumeParserService
+
+async def main():
+    s = ResumeParserService()
+    print('pdftotext_cmd=', s._pdftotext_cmd)
+    t = await s.extract_text('uploads/resumes/user_6_b1261b08ff664c86910c67311018cd2a.pdf', 'application/pdf')
+    print('len=', len(t))
+    print(t[:400])
+
+asyncio.run(main())
